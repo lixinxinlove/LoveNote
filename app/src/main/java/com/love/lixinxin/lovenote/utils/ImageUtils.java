326df04d782f7e10d1b80180904fcd78f3109d9f
lixinxinlove <895330766@qq.com>
2018-02-19 13:36:35 +0800

    添加图片工具类

---
+++ app/src/main/java/com/love/lixinxin/lovenote/utils/ImageUtils.java
@@ -0,0 +1,35 @@
+package com.love.lixinxin.lovenote.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+/**
+ * Created by lixinxin on 2018/2/19.
+ */
+
+public class ImageUtils {
+
+
+    public static void loadImage(Context context,ImageView imageView, int imgId){
+
+        Glide.with(context).load(imgId).into(imageView);
+    }
+
+    public static void loadImage(Context context,ImageView imageView, String url) {
+        Glide.with(context).load(url).into(imageView);
+    }
+
+    public static void loadImage(Context context,ImageView imageView, String url,int defImg){
+        RequestOptions options=new RequestOptions();
+        options.placeholder(defImg);
+        options.error(defImg);
+        Glide.with(context).load(url).apply(options).into(imageView);
+    }
+
+
+
+
+}
